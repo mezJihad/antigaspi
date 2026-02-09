ae9d42ef0982cbfdf34a97bc804892add74347b9
mezouari jihad <jihadm@algoconsulting.net>
2026-02-09 11:19:46 +0100

    update

--- backend-dotnet/Antigaspi.Infrastructure/Persistence/AntigaspiDbContext.cs
+++ backend-dotnet/Antigaspi.Infrastructure/Persistence/AntigaspiDbContext.cs
@@ -16,9 +16,22 @@ public class AntigaspiDbContext : DbContext
     public DbSet<City> Cities { get; set; }
 
 
+    protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+    {
+        configurationBuilder.Properties<DateTime>().HaveConversion<UtcDateTimeConverter>();
+    }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
         base.OnModelCreating(modelBuilder);
+    }
+}
+
+public class UtcDateTimeConverter : Microsoft.EntityFrameworkCore.Storage.ValueConversion.ValueConverter<DateTime, DateTime>
+{
+    public UtcDateTimeConverter()
+        : base(v => v.Kind == DateTimeKind.Utc ? v : v.ToUniversalTime(), v => DateTime.SpecifyKind(v, DateTimeKind.Utc))
+    {
     }
 }
